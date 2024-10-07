7fd708ad715740df65ace9f48001d1a26c41c43f
Dinesh-nyx <dinesh.s6302@gmail.com>
2024-10-07 14:57:55 +0530

    Add files via upload

---
+++ din/plain.java
@@ -0,0 +1,30 @@
+public class plain {
+    public static void main(String[] args) {
+        String input = "madam";  // Change input to "madam"
+        
+        if (isPalindrome(input)) {
+            System.out.println("\"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + input + "\" is not a palindrome.");
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        // Remove non-alphanumeric characters and convert to lowercase
+        String cleanedStr = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        
+        // Initialize pointers
+        int left = 0;
+        int right = cleanedStr.length() - 1;
+
+        // Check for palindrome
+        while (left < right) {
+            if (cleanedStr.charAt(left) != cleanedStr.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
---
+++ din/t.java
@@ -0,0 +1,12 @@
+
+class t
+{
+public static void main(String args[])
+{
+int i;
+String m[ ]={"Anith","Ben","Rajiv","Vijay","Zafa"};
+System.out.println("The names are :");
+for(i=0;i<5;i++)
+System.out.println(m[i]);
+}
+}
